f8177242f7f871814b9496613e14168e63cc1be5
Alex <86080211+AlexPushilov@users.noreply.github.com>
2023-02-15 13:08:04 +0300

    Update Electronic_Diary_Telegram_Bot_Project.py

--- Electronic_Diary_Telegram_Bot_Project.py
+++ Electronic_Diary_Telegram_Bot_Project.py
@@ -95,7 +95,7 @@ def daily_answer(message):
         date = week_days[dt.weekday()]
         time2 = datetime.datetime.now(timezone("Europe/Moscow")).time()
         k = datetime.time(0, 0)
-        our_bot.send_message(message.chat.id, f"Текущее время {time2}")
+        our_bot.send_message(message.chat.id, f"Текущее время и дата: {time2}, {dt.weekday()}")
         if date == "now_sunday":
             our_bot.send_message(message.chat.id, "Сегодня можно отдохнуть")
         if time2 < timetable["lesson_1"][0]:
