a3ac096aaf282d731c8ef8b062ace7eeb6d771f0
Alex <86080211+AlexPushilov@users.noreply.github.com>
2023-02-15 11:54:28 +0300

    Update Electronic_Diary_Telegram_Bot_Project.py

--- Electronic_Diary_Telegram_Bot_Project.py
+++ Electronic_Diary_Telegram_Bot_Project.py
@@ -93,6 +93,7 @@ def daily_answer(message):
     if message.text == "Текущий и следующий урок":
         dt = datetime.datetime.now(timezone("Europe/Moscow"))
         date = week_days[dt.weekday()]
+        print(dt)
         time2 = datetime.datetime.now(timezone("Europe/Moscow")).time()
         k = datetime.time(0, 0)
         if date == "now_sunday":
